542bf6455d5ffd8b7bcf41d540675dffbc904ffd
alelsandr <sanya10081999@gmail.com>
2025-07-25 16:30:52 +0700

    feat: added SubcategoryInline for Category

--- dds_service/apps/categories/admin.py
+++ dds_service/apps/categories/admin.py
@@ -3,15 +3,25 @@ from django.contrib import admin
 from apps.categories.models import Category, SubCategory
 
 
+class SubCategoryInline(admin.TabularInline):
+    model = SubCategory
+    extra = 0
+    max_num = 5
+    fields = ('name',)
+
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    inlines = (SubCategoryInline,)
+    required_fields = ("record_type", "name")
     list_display = (
         "name",
+        "record_type"
     )
-
     search_fields = (
         "name",
     )
+
 
 @admin.register(SubCategory)
 class SubCategoryAdmin(admin.ModelAdmin):
@@ -19,7 +29,6 @@ class SubCategoryAdmin(admin.ModelAdmin):
         "name",
         "category",
     )
-
     search_fields = (
         "name",
     )
